116e7f65c703c7eec556e9a9286e723eb1e35ad1
Hussein(Shawn) El-Souri <helsouri@umich.edu>
2018-10-30 20:35:42 -0400

    Delete pstat.h

--- pstat.h
+++
@@ -1,8 +0,0 @@
-#include "param.h"
-
-struct pstat {
-    int inuse[NPROC]; // whether this slot of the process table is in use (1 or 0)
-    int pid[NPROC];   // PID of each process
-    int priority[NPROC]; // current priority level of each process (1-6)
-    int ticks[NPROC][6]; // number of ticks each process has accumulated at each of 6 priorities
-};
